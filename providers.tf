a9b5b652b6c22fd4e31c8ca3dcf8204d4ae11ea5
SlvrOwl <renjith_willis@yahoo.com>
2026-06-25 22:00:58 +1200

    initial setup: add providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+  }
+}
+
+provider "azuread" {
+}
